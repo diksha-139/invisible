4a23e272353e05474995fe1cfef371e61a561e5b
diksha-139 <64410938+diksha-139@users.noreply.github.com>
2024-06-20 20:47:35 +0530

    Update invisible_cloak.py

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -44,25 +44,27 @@ while (cap.isOpened()):
 
     #Open and expand the image where there is mask 1 (color)
     mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-   
+    mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
 
     #Selecting only the part that does not have mask one and saving in mask 2
+    mask_2 = cv2.bitwise_not(mask_1)
    
 
     #Keeping only the part of the images without the red color 
     #(or any other color you may choose)
-    
+    res_1 = cv2.bitwise_and(img, img, mask=mask_2)
 
     #Keeping only the part of the images with the red color
     #(or any other color you may choose)
-    
+    res_2 = cv2.bitwise_and(bg, bg, mask=mask_1)
 
     #Generating the final output by merging res_1 and res_2
-    
-    
+    final_output = cv2.addWeighted(res_1, 1, res_2, 1, 0)
+    output_file.write(final_output)
     #Displaying the output to the user
-    cv2.imshow("magic", )
-    cv2.waitKey(1)
+    cv2.imshow("magic", final_output)
+    if cv2.waitKey(1)== 32:
+        break
 
 
 cap.release()
